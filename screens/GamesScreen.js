4fc0101ce79e1aebde40e698451a0c2b4ee44884
Kyle Lee <Kylecrlee95@gmail.com>
2019-03-07 20:07:21 -0600

    implemented OtherGames and GamesScreen into app. Need to fix a componentWillMountError & this.setState in GamesScreen

--- screens/GamesScreen.js
+++ screens/GamesScreen.js
@@ -1,3 +1,21 @@
+import React from "react";
+import { ThemeProvider } from "react-native-elements";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Button
+} from "react-native";
+import data from "../constants/dopestatz";
+import { WebBrowser } from "expo";
+import { MonoText } from "../components/StyledText";
+import { TopMatch } from "../components/TopMatch.js";
+import { OtherGames } from "../components/OtherGames.js";
+
 export default class GamesScreen extends React.Component {
   constructor() {
     super();
@@ -103,14 +121,108 @@ export default class GamesScreen extends React.Component {
           </ScrollView>
 
           <View style={styles.tabBarInfoContainer}>
-            {/* <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text> */}
-
-            {/* <View
-            style={[styles.codeHighlightContainer, styles.navigationFilename]}
-          /> */}
+            <Text style={styles.tabBarInfoText}>
+              This is a tab bar. You can edit it in:
+            </Text>{" "}
+            <View
+              style={[styles.codeHighlightContainer, styles.navigationFilename]}
+            />
           </View>
         </View>
       </ThemeProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: "rgba(0,0,0,0.4)",
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: "center"
+  },
+  contentContainer: {
+    paddingTop: 30
+  },
+  welcomeContainer: {
+    alignItems: "center",
+    marginTop: 10,
+    marginBottom: 20
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: "contain",
+    marginTop: 3,
+    marginLeft: -10
+  },
+  getStartedContainer: {
+    alignItems: "center",
+    marginHorizontal: 50
+  },
+  homeScreenFilename: {
+    marginVertical: 7
+  },
+  codeHighlightText: {
+    color: "rgba(96,100,109, 0.8)"
+  },
+  codeHighlightContainer: {
+    backgroundColor: "rgba(0,0,0,0.05)",
+    borderRadius: 3,
+    paddingHorizontal: 4
+  },
+  getStartedText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    lineHeight: 24,
+    textAlign: "center"
+  },
+  tabBarInfoContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOffset: { height: -3 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3
+      },
+      android: {
+        elevation: 20
+      }
+    }),
+    alignItems: "center",
+    backgroundColor: "#fbfbfb",
+    paddingVertical: 20
+  },
+  tabBarInfoText: {
+    fontSize: 17,
+    color: "rgba(96,100,109, 1)",
+    textAlign: "center"
+  },
+  navigationFilename: {
+    marginTop: 5
+  },
+  helpContainer: {
+    marginTop: 15,
+    alignItems: "center"
+  },
+  helpLink: {
+    paddingVertical: 15
+  },
+  helpLinkText: {
+    fontSize: 14,
+    color: "#2e78b7"
+  },
+  teams: {
+    fontSize: 25,
+    fontWeight: "bold"
+  }
+});
