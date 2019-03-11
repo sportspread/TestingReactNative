229f9f6c6ae15ec7bbf01d1a142a9d530f64347c
Sam Bengtson <sambeng1@gmail.com>
2019-03-11 12:57:38 -0500

    Fixed object loop error

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -48,34 +48,36 @@ export default class HomeScreen extends React.Component {
 
   DO NOT DELETE
   */
-    fetch(
-      'https://therundown-therundown-v1.p.rapidapi.com/sports/4/events?include=scores+or+teams+or+all_periods',
-      {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': SamsKey,
-        },
-      }
-    )
-      .then(res => {
-        return res.json()
-      })
-      .then(resJSON => {
-        let today = new Date()
-        let tomorrow = new Date()
-        tomorrow.setDate(today.getDate() + 1)
-        let filtered = resJSON.events.filter(event => {
-          console.log()
-          return (
-            Date.parse(event.event_date.slice(0, 10)) < Date.parse(tomorrow)
-          )
-        })
-        //Won't allow you to setState on an unmounted component. Will need to store this data in a constant and then calll setState on componentDidMount
-        return filtered
-      })
-      .then(filtered => {
-        this.setState({ allGamesData: filtered })
-      })
+
+    // fetch(
+    //   'https://therundown-therundown-v1.p.rapidapi.com/sports/4/events?include=scores+or+teams+or+all_periods',
+    //   {
+    //     method: 'GET',
+    //     headers: {
+    //       'X-RapidAPI-Key': SamsKey,
+    //     },
+    //   }
+    // )
+    //   .then(res => {
+    //     return res.json()
+    //   })
+    //   .then(resJSON => {
+    //     let today = new Date()
+    //     let tomorrow = new Date()
+    //     tomorrow.setDate(today.getDate() + 1)
+    //     let filtered = resJSON.events.filter(event => {
+    //       console.log()
+    //       return (
+    //         Date.parse(event.event_date.slice(0, 10)) < Date.parse(tomorrow)
+    //       )
+    //     })
+    //     //Won't allow you to setState on an unmounted component. Will need to store this data in a constant and then calll setState on componentDidMount
+    //     return filtered
+    //   })
+    //   .then(filtered => {
+    //     this.setState({ allGamesData: filtered })
+    //   })
+
     /* 
   Below here is for testing using dummy data
 
@@ -83,7 +85,8 @@ export default class HomeScreen extends React.Component {
 
   dOnT TeLl Me WhAt To Do
   */
-    // this.setState({ allGamesData: data.events })
+
+    this.setState({ allGamesData: data.events })
   }
   scrollToTop() {
     this.scroller.scrollTo({ x: 0, y: 0 })
@@ -130,12 +133,14 @@ export default class HomeScreen extends React.Component {
     let counter = 0
     let total = 0
     for (key in event.lines) {
-      let curSpread =
-        event.lines[key].spread.point_spread_home > 0
-          ? event.lines[key].spread.point_spread_home
-          : event.lines[key].spread.point_spread_home * -1
-      total += curSpread
-      counter++
+      if (event.lines.hasOwnProperty([key])) {
+        let curSpread =
+          event.lines[key].spread.point_spread_home > 0
+            ? event.lines[key].spread.point_spread_home
+            : event.lines[key].spread.point_spread_home * -1
+        total += curSpread
+        counter++
+      }
     }
     return total / counter
   }
@@ -176,6 +181,7 @@ export default class HomeScreen extends React.Component {
     }
 
     let sorted = this.bubbleSort(teamsAndSpreads)
+    console.log(sorted)
     this.scrollToTop()
     this.setState({
       bestGame: sorted.shift(),
